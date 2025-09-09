b8146f0c6948a66fead8af4d6127385b1abbf1ae
Tugiti Likhitha <tugitilikhitha.24@kgpian.iitkgp.ac.in>
2025-09-09 14:32:47 +0530

    Update __init__.py

--- valid8/blockchain/__init__.py
+++ valid8/blockchain/__init__.py
@@ -1,1 +1,22 @@
-"""Utility modules package"""
+"""
+Certificate Verification System Utilities Package
+
+This package contains utility modules for:
+- OCR processing with Tesseract
+- Watermark detection using OpenCV  
+- Digital signature verification
+- DigiLocker API integration
+"""
+
+from .ocr_processor import OCRProcessor
+from .watermark_detector import WatermarkDetector
+from .signature_verifier import SignatureVerifier
+from .digilocker_api import DigilockerAPI
+
+__all__ = [
+    'OCRProcessor',
+    'WatermarkDetector', 
+    'SignatureVerifier',
+    'DigilockerAPI'
+]
+
